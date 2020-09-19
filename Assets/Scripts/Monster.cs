31206e9e17c466c791d42c3ba039b6fcd047e15b
Rzzil <37683359+Rzzil@users.noreply.github.com>
2020-09-19 18:33:37 +1000

    Finish

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private IEnumerator ChangeState()
+    {
+        int i = 1;
+        while (true)
+        {
+            animator.SetInteger("state", i);
+            i++;
+            if (i >= 7)
+                i = 0;
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
---
+++ Assets/Scripts/PacmanMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMovement : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private List<int> MoveList;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Movement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move(int state)
+    {
+        animator.SetInteger("state", state);
+        transform.Translate(Vector2.right * 0.55f);
+    }
+
+    private IEnumerator Movement()
+    {
+        int i = 0;
+        while (true)
+        {
+            Move(MoveList[i]);
+            i++;
+            if (i >= MoveList.Count)
+                i = 0;
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+}
